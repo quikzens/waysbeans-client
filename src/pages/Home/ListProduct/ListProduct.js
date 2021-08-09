9b04fd1b920be7b61d698f5a2e748309992c6e12
quikzens <febrianamir5@gmail.com>
2021-08-09 22:04:45 +0800

    filter done

--- src/pages/Home/ListProduct/ListProduct.js
+++ src/pages/Home/ListProduct/ListProduct.js
@@ -12,6 +12,7 @@ const ListProduct = () => {
 
   // init state
   const [products, setProducts] = useState(null)
+  const [maxPrice, setMaxPrice] = useState('')
 
   // init lifecycle
   useEffect(() => {
@@ -21,6 +22,19 @@ const ListProduct = () => {
     }
   }, [dataProducts])
 
+  const applyFilter = () => {
+    setProducts((prevProducts) => {
+      return prevProducts.filter((product) => {
+        return product.price <= maxPrice
+      })
+    })
+  }
+
+  const cleanFilter = () => {
+    setProducts(dataProducts)
+    setMaxPrice('')
+  }
+
   if (!products) return <Loading />
 
   return (
@@ -29,10 +43,19 @@ const ListProduct = () => {
         <h2>Filter product based on price</h2>
         <div className='flex gap-1'>
           <div className='form-item'>
-            <input type='text' placeholder='max price' />
+            <input
+              type='text'
+              placeholder='max price'
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
           </div>
-          <button className='btn'>Filter</button>
-          <button className='btn'>Clean Filter</button>
+          <button className='btn' onClick={applyFilter}>
+            Filter
+          </button>
+          <button className='btn' onClick={cleanFilter}>
+            Clean Filter
+          </button>
         </div>
       </div>
       <div className='home-list-product container'>
