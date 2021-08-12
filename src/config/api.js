c4d3bd49ff4666377613de9ddb18f01efa50b272
quikzens <febrianamir5@gmail.com>
2021-08-12 19:23:34 +0800

    configure api url

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export const API = axios.create({
   baseURL: 'https://waysbeans-api.herokuapp.com/api/v1',
-  // baseURL: 'http://localhost:3000/api/v1',
 })
 
 export const setAuthToken = (token) => {
